05f4e6f2d39e48551ca3bd251f3f4d45d516863f
klsn <git@su-shiki.com>
2021-10-03 18:45:08 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	bool bingo = false;
+	vector<vector<vector<int>>> hit(5, vector<vector<int>>(5, vector<int>(5)));
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++) for (int k=0;k<5;k++){
+		cin >> hit.at(i).at(j).at(k);
+	}
+
+
+//縦横奥
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++) for (int k=0;k<5;k++){
+		if (hit.at(i).at(j).at(k) != 1) break;
+		if (k == 4) bingo = true; 
+	}
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++) for (int k=0;k<5;k++){
+		if (hit.at(i).at(k).at(j) != 1) break;
+		if (k == 4) bingo = true; 
+	}
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++) for (int k=0;k<5;k++){
+		if (hit.at(k).at(i).at(j) != 1) break;
+		if (k == 4) bingo = true; 
+	}
+
+//斜め
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++){
+		if (hit.at(i).at(j).at(j) != 1) break;
+		if (j == 4) bingo = true; 
+	}
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++){
+		if (hit.at(i).at(j).at(4-j) != 1) break;
+		if (j == 4) bingo = true; 
+	}
+
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++){
+		if (hit.at(j).at(i).at(j) != 1) break;
+		if (j == 4) bingo = true; 
+	}
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++){
+		if (hit.at(j).at(i).at(4-j) != 1) break;
+		if (j == 4) bingo = true; 
+	}
+
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++){
+		if (hit.at(j).at(j).at(i) != 1) break;
+		if (j == 4) bingo = true; 
+	}
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++){
+		if (hit.at(j).at(4-j).at(i) != 1) break;
+		if (j == 4) bingo = true; 
+	}
+
+
+	for (int i=0;i<5;i++) {
+		if (hit.at(i).at(i).at(i) != 1) break;
+		if (i == 4) bingo = true; 
+	}
+	for (int i=0;i<5;i++) for (int j=0;j<5;j++){
+		if (hit.at(i).at(i).at(4-j) != 1) break;
+		if (i == 4) bingo = true; 
+	}
+
+
+
+
+	cout << bingo <<endl;
+
+}
